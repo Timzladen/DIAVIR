6398f98913d40c05f802e7c3eb2c37dceaaba0ea
Timzladen <turk.tim@siol.net>
2026-04-17 11:53:57 +0200

    Update assembly_and_annotation_workflow.sh

--- bacterial_metaT/assembly_and_annotation_workflow.sh
+++ bacterial_metaT/assembly_and_annotation_workflow.sh
@@ -2,7 +2,7 @@
 
 #run the assembly on CLC Genomics QCed and trimmed fastq files
 conda activate spades
-bash assembly.sh
+bash 01_assembly.sh
 conda deactivate
 
 #predict ORFs
@@ -12,11 +12,11 @@ TransDecoder.Predict -t merged_transcripts.fasta
 conda deactivate
 #cluster contigs
 conda activate cd-hit
-bash cluster.sh
+bash 02_cluster.sh
 conda deactivate
 #run salmon read quantification
 conda activate salmon
-bash salmon.sh
+bash 03_salmon.sh
 conda deactivate
 
 #annotate contigs with eggnog
